8027692e064905a093506fdcb20ad2d71261e0fa
Zbelli <belli.zack@gmail.com>
2024-09-01 13:03:25 -0400

    2. refactor of while loop

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from constants import *
 
 
 def main():
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
 
     while True:
-        pygame.Surface.fill(screen,(0,0,0))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
