f522a40a9d6b53d72b75a26195b9ef86c6613984
Zbelli <belli.zack@gmail.com>
2024-09-02 08:46:13 -0400

    4. style cleanup

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen,(0,0,0))
+        pygame.Surface.fill("black")
         pygame.display.flip()
-        clock.tick(60)
+
+        # limit the framerate to 60 FPS
+        clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
